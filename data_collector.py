e527028ef91d54250961139f23bd963f35c82919
Kallol Saha <kallolsaharesearch@gmail.com>
2024-12-20 00:20:22 -0500

    completed demo collection pipeline from video

--- data_collector.py
+++ data_collector.py
@@ -1,25 +1,56 @@
 import heapq
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 import open3d as o3d
+from tqdm import tqdm
 
 DIFF_THRESH = 3.            # What is the point track difference below which object is considered stationary
 STATIONARY_THRESH = 4       # What is the minimum change in timesteps between different stationary frames for an object to be considered transformed
 
-def plot_pcd(pts3d):
+# def plot_pcd(pts3d):
+
+#     pcd = np.zeros_like(pts3d, dtype = np.float64)
+#     pcd[:, :] = pts3d[:, :]
+#     pts_vis = o3d.geometry.PointCloud()
+#     pts_vis.points = o3d.utility.Vector3dVector(pcd)
+
+#     return [pts_vis]
+
+
+def plot_pcd(pts3d, pcd_seg = None):
 
     pcd = np.zeros_like(pts3d, dtype = np.float64)
     pcd[:, :] = pts3d[:, :]
+    
     pts_vis = o3d.geometry.PointCloud()
     pts_vis.points = o3d.utility.Vector3dVector(pcd)
+    
+    if pcd_seg is not None:    
 
-    return [pts_vis]
+        seg_ids = np.unique(pcd_seg)
+        n = len(seg_ids)
+        cmap = plt.get_cmap("tab10")
+        id_to_color = {uid: cmap(i / n)[:3] for i, uid in enumerate(seg_ids)}
+        colors = np.array([id_to_color[seg_id] for seg_id in pcd_seg])
+        # print("Seg IDs = ", seg_ids)
+        # print("Colors = ", id_to_color)
+        pts_vis.colors = o3d.utility.Vector3dVector(colors)
 
-class DataCollecter:
+    o3d.visualization.draw_geometries([pts_vis])
 
-    def __init__(self):
+class DataCollector:
 
-        pass
+    def __init__(self, folder_path):
+
+        # Prepare data folder:
+        self.folder_path = folder_path + "/"
+        os.makedirs(self.folder_path, exist_ok=True)
+        os.makedirs(self.folder_path + "train/", exist_ok=True)
+        os.makedirs(self.folder_path + "test/", exist_ok=True)
+
+        self.train_demos = len(os.listdir(self.folder_path + "train/"))
+        self.test_demos = len(os.listdir(self.folder_path + "test/"))
 
     def get_indices(self, pred_tracks):
         """
@@ -115,18 +146,107 @@ class DataCollecter:
         # Length of change_objects will always be one less than indices, because no more objects are moved at the last index.
         return np.array(indices), change_objects
     
-    def get_transformation_between_points(self, pts1, pts2):
+    def get_transformation_between_points(self, P, Q):
+        """
+        P => (n, 3)
+        Q => (n, 3)
+        """
 
-        pass
+        # Compute centroids
+        P_mean = np.mean(P, axis=0)
+        Q_mean = np.mean(Q, axis=0)
+        
+        # Center the points
+        P_centered = P - P_mean
+        Q_centered = Q - Q_mean
+        
+        # Compute cross-covariance matrix
+        H = P_centered.T @ Q_centered
+        
+        # Perform SVD
+        U, S, Vt = np.linalg.svd(H)
+
+        # Compute rotation matrix
+        R = Vt.T @ U.T
+        if np.linalg.det(R) < 0:
+            Vt[2, :] *= -1
+            R = Vt.T @ U.T
+
+        # Compute translation vector
+        t = Q_mean - R @ P_mean
+        
+        # Form transformation matrix
+        T = np.eye(4)
+        T[:3, :3] = R
+        T[:3, 3] = t
+        
+        return R, t, T
     
-    def prepare_data(self, indices, objects, object_tracks, pcd_sequence):
+    def ransac_find_transformation(self, P, Q, sample_size = 100, threshold=0.1, max_iterations=100, inlier_ratio=0.5):
+        best_R = None
+        best_t = None
+        best_inliers = []
+        n_points = len(P)
+        
+        for _ in range(max_iterations):
+            # Randomly sample a subset of correspondences
+            indices = np.random.choice(n_points, sample_size, replace=False)
+            P_sample = P[indices]
+            Q_sample = Q[indices]
+            
+            # Estimate transformation using the subset
+            R, t, _ = self.get_transformation_between_points(P_sample, Q_sample)
+            
+            # Transform all points and calculate residuals
+            P_transformed = (R @ P.T).T + t
+            residuals = np.linalg.norm(P_transformed - Q, axis=1)
+            
+            # Determine inliers
+            inliers = np.where(residuals < threshold)[0]
+            
+            # Keep the model with the most inliers
+            if len(inliers) > len(best_inliers):
+                best_inliers = inliers
+                best_R = R
+                best_t = t
+        
+        # Recompute the transformation using all inliers
+        if len(best_inliers) > 3:
+            P_inliers = P[best_inliers]
+            Q_inliers = Q[best_inliers]
+            best_R, best_t, _ = self.get_transformation_between_points(P_inliers, Q_inliers)
+
+        # Form transformation matrix
+        T = np.eye(4)
+        T[:3, :3] = best_R
+        T[:3, 3] = best_t
+        
+        return T
+        
+    def transform_pcd(self, pcd, T):
+
+        # Convert to homogeneous coordinates
+        ones = np.ones((pcd.shape[0], 1))
+        pcd_h = np.hstack((pcd, ones))  # (n, 4)
+
+        # Apply transformation
+        pcd_transformed_h = (T @ pcd_h.T).T  # (n, 4)
+        
+        # Convert back to Cartesian coordinates
+        pcd_transformed = pcd_transformed_h[:, :3]
+
+        return pcd_transformed
+    
+    def get_transform_sequence(self, indices, objects, object_tracks, pcd_sequence):
         """
         object_tracks => list of (frames, points, 2) arrays
         """
 
         # In object tracks last axis, the 0th index is width (1280), the 1st index is height (720)
+
+        transforms = np.zeros((len(objects), 4, 4))
         
-        for i in range(len(indices) - 1):
+        for i in tqdm(range(len(indices) - 1)):
 
             track = object_tracks[objects[i]]       # Get the point track of the object being moved
 
@@ -137,12 +257,94 @@ class DataCollecter:
             pts3d_after = pcd_sequence[indices[i+1], pts2d_after[:, 1], pts2d_after[:, 0]]
             # I've done a visualization check on the 3d points before and after, we are good to go for svd !!
 
-            # geometries = plot_pcd(np.concatenate([pts3d_before, pts3d_after], axis = 0))
-            # o3d.visualization.draw_geometries(geometries)
+            # plot_pcd(np.concatenate([pts3d_before, pts3d_after], axis = 0))
 
+            T = self.ransac_find_transformation(pts3d_before, pts3d_after, threshold = 0.02)
+            # I've done a visualization check for the transform. But, TODO: needs to be robust to outliers (see result in progress report)
             
+            # transformed_pcd = self.transform_pcd(pts3d_before, T)
+            # seg = np.zeros((2 * pts3d_before.shape[0],), dtype = np.int64)
+            # seg[pts3d_before.shape[0]:] = 1
+            # plot_pcd(np.concatenate([transformed_pcd, pts3d_after], axis = 0), seg)
 
-            print("")
+            transforms[i] = T[:]
+
+        return transforms
+    
+    def prepare_initial_pcd(self, masks, pcd_sequence, vis_threshold = 1.):
+        """
+        masks => (num_objects, 720, 1280)
+        """
+
+        num_objects = masks.shape[0]
+        
+        initial_pcd = pcd_sequence[0].reshape((-1, 3))
+
+        weights = np.arange(num_objects) + 1
+        mask_sum = np.tensordot(weights, masks, axes = ([0], [0])) - 1      # Objects get 0 to n segmentation, everything else gets a -1
+        initial_pcd_seg = mask_sum.reshape((-1, ))
+
+        vis_mask = initial_pcd[:, -1] < vis_threshold
+
+        pcd = initial_pcd[vis_mask]
+        pcd_seg = initial_pcd_seg[vis_mask]
+
+        # Downsample environment points to equal object points:
+        env_mask = (pcd_seg == -1)
+        num_obj_points = (pcd_seg != -1).sum()
+        num_env_points = env_mask.sum()
+
+        sample_env_mask = np.array(([True] * num_obj_points) + ([False] * (num_env_points - num_obj_points)))
+        np.random.shuffle(sample_env_mask)
+
+        env_mask[env_mask] = env_mask[env_mask] & sample_env_mask
+
+        downsample_mask = env_mask | (pcd_seg != -1)
+        pcd = pcd[downsample_mask]
+        pcd_seg = pcd_seg[downsample_mask]
+
+        # plot_pcd(pcd, pcd_seg)
+
+        return pcd, pcd_seg
+    
+    def save_final_data(self, initial_pcd, initial_pcd_seg, transforms, objects, initial_rgb, mode = "train"):
+        
+        prev_pcd = initial_pcd.copy()
+        pcd_seg = initial_pcd_seg.copy()
+        
+        for i in range(len(transforms)):
+
+            obj_mask = (pcd_seg == objects[i])
+            
+            transformed_pcd = prev_pcd.copy()
+            transformed_pcd[obj_mask] = self.transform_pcd(transformed_pcd[obj_mask], transforms[i])
+
+            classes = np.where(obj_mask, 0, 1)
+
+            # plot_pcd(transformed_pcd, pcd_seg)
+            # plot_pcd(transformed_pcd, classes)
+        
+            # TODO: Remember to transform to robot frame (do we need this? Because taxposeD applies random transforms anyways)
+        
+            if mode == "train":
+                np.savez(
+                        self.folder_path + str(mode) + "/" + str(self.train_demos) + "_teleport_obj_points.npz",
+                        clouds=transformed_pcd,
+                        masks=pcd_seg,
+                        classes=classes,
+                    )
+                self.train_demos += 1
+                
+            if mode == "test":
+                np.savez(
+                        self.folder_path + str(mode) + "/" + str(self.test_demos) + "_teleport_obj_points.npz",
+                        clouds=transformed_pcd,
+                        masks=pcd_seg,
+                        classes=classes,
+                    )
+                self.test_demos += 1
+
+            prev_pcd = transformed_pcd.copy()
     
     def visualize(self):
 
--- generate_data.py
+++ generate_data.py
@@ -1,13 +1,15 @@
 from gsam_wrapper import GSAM2
 from cotracker_wrapper import Cotracker3
-from data_collector import DataCollecter
+from data_collector import DataCollector
 import numpy as np
 from tqdm import tqdm
 
 object_names = "rubber duck. blue box. wooden bowl"
 video_path = "assets/videos/example1/rgb_vid.mp4"
+data_path = "assets/data/example1"
 frame = 0
 device = "cuda:1"
+vis_threshold = 1.
 
 # Load point cloud data:
 pcd_sequence = np.load("assets/videos/example1/pcd_vid.npy")
@@ -16,35 +18,33 @@ rgb_sequence = np.load("assets/videos/example1/rgb_vid.npy")
 # Instantiate modules
 gsam2 = GSAM2(device)
 cotracker3 = Cotracker3(device)
-data_collector = DataCollecter()
+data_collector = DataCollector(data_path)
 
 # GSAM inference
 masks, scores, logits, confidences, labels, input_boxes = gsam2.get_masks(object_names, video_path, frame)
 gsam2.visualize(video_path, masks, confidences, labels, input_boxes, frame)
 
+# Get the initial point cloud:
+initial_pcd, initial_pcd_seg = data_collector.prepare_initial_pcd(masks, pcd_sequence)
+
 # Cotracker inference
-masks = masks[:, 0]     # TODO: Generalize to multi-object
-
+masks = masks[:, 0]
 object_tracks = []
-
 # for i in tqdm(range(masks.shape[0])):
 #     pred_tracks, pred_visibility = cotracker3.get_tracks(video_path, masks[i])
 #     pred_tracks = pred_tracks.cpu().detach().numpy()
 #     pred_visibility = pred_visibility.cpu().detach().numpy()
 #     object_tracks.append(pred_tracks[0])
     # cotracker3.visualize(video_path, pred_tracks, pred_visibility, filename = str(i))
-
 for i in range(3):
-
     object_tracks.append(np.load(str(i) + ".npy"))
 
 # Detect movement:
 indices, objects = data_collector.detect_movement(object_tracks)
-data_collector.prepare_data(indices, objects, object_tracks, pcd_sequence)
 
-pred_tracks = pred_tracks[0]            # TODO: Generalize to multi-object
-pred_visibility = pred_visibility[0]
+# Get transform sequence:
+transforms = data_collector.get_transform_sequence(indices, objects, object_tracks, pcd_sequence)
 
-indices = data_collector.get_indices(pred_tracks)
+# Save the data:
+data_collector.save_final_data(initial_pcd, initial_pcd_seg, transforms, objects, rgb_sequence[0], mode = "train")
 
-print("Done")
